4258ba18b3ce565a69ef8ec9ee77d9c586790bec
Apelng <administrator@apelasset.com>
2025-09-22 11:40:31 +0100

    dded package.json each

--- index.js
+++ index.js
@@ -78,9 +78,9 @@ app.use(cors(corsOptions));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
