0355bf1b3dc633a8c2e503deac2f73f15c28f5f4
Apelng <administrator@apelasset.com>
2026-01-28 10:54:30 +0100

    updated linkage e-rights

--- routes/forms.js
+++ routes/forms.js
@@ -101,7 +101,7 @@ async function generateRightsPdfBuffer(formData) {
     // Load PDF template
     if (false && process.env.NODE_ENV === 'production') {
       const cloudinary = require('../config/cloudinary');
-      const templateUrl = cloudinary.url('rights-submissions/rights-form/TIP_RIGHTS_ISSUE', { format: 'pdf' });
+      const templateUrl = cloudinary.url('rights-submissions/rights-form/LINKAGE_RIGHTS_ISSUE', { format: 'pdf' });
       const response = await fetch(templateUrl);
       if (!response.ok) {
         throw new Error(`Failed to fetch PDF template: ${response.status} ${response.statusText}`);
